d2755c20daf89b6cbcc02596e45eb53a8963702c
igormjq <igor.quevedo@nodo.cc>
2017-09-18 13:32:17 -0300

    vue resource added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import Vuex from 'vuex'
 
 // Filters
@@ -15,7 +16,11 @@ import { store } from './store/store'
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 Vue.use(Vuex);
+
+Vue.http.root = 'https://vuejs-stock-trader-346f5.firebaseio.com/';
+
 
 const router = new VueRouter({
   mode: 'history',
